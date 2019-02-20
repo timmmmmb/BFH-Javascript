65a2e31a2996571966a85b90a71f07b24e454559
Tim Frey <frey.tim@outlook.com>
2019-02-20 09:46:36 +0100

    you can now convert from morse back

--- exercise/Morse01/script.js
+++ exercise/Morse01/script.js
@@ -3,15 +3,24 @@ var alphabetString =
 "m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;" +
 "y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..;!=-.-.--";
 
-console.log(convertToMorse("test"));
+console.log(convertFromMorse(".. // .- --. .-. . . // -.. ..- -.. ."));
 
+/**
+ * replaces all occurences of the search string with the replacment string in the input 
+ * @param {*} string input string which shall be searchd in 
+ * @param {*} search the string that should get replaced
+ * @param {*} replacement the new string that replaces the search string
+ */
+function replaceAll(string, search, replacement) {
+    return string.split(search).join(replacement);
+};
 
 /**
  * this function converts one character to morse
  * @param {*} input one character that should get converted to morse
  */
 function charToMorseCode(input){
-    var output = alphabetString.match(input+"=[.-]*");
+    var output = alphabetString.match(input+"=[.-/]*");
     output = output[0].substring(2);
     return output;
 }
@@ -24,6 +33,31 @@ function convertToMorse(input){
     let output = "";
     for (var i = 0; i < input.length; i++) {
         output += charToMorseCode(input.charAt(i));
+        output += " ";
     }
+    return output;
+}
+
+/**
+ * converts a string from morse
+ * @param {*} input a string that should get converted from morse
+ */
+function convertFromMorse(input){
+    let output = "";
+    input = replaceAll(input,".","\\.");
+    input = input.split(" ");
+    for (var i = 0; i < input.length; i++) {
+        output += charFromMorseCode(input[i]);
+    }
+    return output;
+}
+
+/**
+ * this function converts one character from morse
+ * @param {*} input one character that should get converted from morse
+ */
+function charFromMorseCode(input){
+    var output = alphabetString.match(".="+input+";");
+    output = output[0].substring(0,1);
     return output;
 }
