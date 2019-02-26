2a36786a4e92eb0a271017af736e5496c430f5a7
Tim Frey <frey.tim@outlook.com>
2019-02-26 18:20:36 +0100

    finished first task

---
+++ exercise/Exercise02/script.js
@@ -0,0 +1,9 @@
+/**
+ * constructor for a Char
+ * @param {*} value the char value
+ * @param {*} string the chars representation as a string
+ */
+function Char(value, string){
+    this.string = string;
+    this.value = value;
+}
