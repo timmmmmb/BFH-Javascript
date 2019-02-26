28654c08d6d22c98c44351a3b8805ffd7ae5ed55
Tim Frey <frey.tim@outlook.com>
2019-02-26 19:35:53 +0100

    finished the whole second exercise

--- exercise/Exercise02/script.js
+++ exercise/Exercise02/script.js
@@ -1,9 +1,79 @@
 /**
  * constructor for a Char
- * @param {*} value the char value
- * @param {*} string the chars representation as a string
+ * @param {Char} value the char value
+ * @param {String} string the chars representation as a string
  */
-function Char(value, string){
+function Char(value, string) {
     this.string = string;
     this.value = value;
+    return this;
 }
+
+/**
+ * constructor for a Font object used to map all chars
+ * @param {String} name name of the font
+ * @param {Char[]} chars an array of chars
+ */
+function Font(name, chars) {
+    this.name = name;
+    var alphabet = new Map();
+
+    // create a HMap from the Char array
+    chars.forEach(function (element) {
+        alphabet.set(element.value, element.string);
+    });
+
+    this.alphabet = alphabet;
+    /**
+     * Function to convert a String using this font
+     * @param {String} text the text that should get renderd
+     */
+    this.render = function(text) {
+        var output = "";
+        for (var i = 0; i < text.length; i++) {
+            output += alphabet.get(text.charAt(i));
+            output += " ";
+        }
+        return output;
+    };
+
+    /**
+     * function to render text and use it as a parameter for a function
+     * @param {String} text the text that should get renderd
+     * @param {Function} to a function that should use the renderd text
+     */
+    this.write = function(text, to) {
+        if (typeof to !== "function") {
+            console.log(this.render(text));
+        } else {
+            to(this.render(text));
+        }
+    };
+
+    return this;
+}
+
+/**
+ * converts a string into an array
+ * the string gets splitted per ;
+ * and it splitts the value, char with =
+ * @param {String} string the string that should get converted into an array
+ */
+function createFontArray(string){
+    let input = string.split(";")
+    let output = [input.length];
+    for(let i = 0; i < input.length; i++){
+        let splitted = input[i].split("=");
+        output[i] =new Char(splitted[0], splitted[1]);
+    }
+    return output;
+}
+
+var alphabetString =
+"a=.-;b=-...;c=-.-.;d=-..;e=.;f=..-;g=--.;h=....;i=..;j=.---;k=-.-;l=.-..;" +
+"m=--;n=-.;o=---;p=.---.;q=--.-;r=.-.;s=...;t=-;u=..-;v=...-;w=.--;x=-..-;" +
+"y=-.-;z=--..; =//;.=.-.-.-;,=--..--;?=..--..;!=-.-.--";
+// create the morsefont
+let morse = new Font("MorseFont",createFontArray(alphabetString));
+// convert some text
+morse.write("hello","");
