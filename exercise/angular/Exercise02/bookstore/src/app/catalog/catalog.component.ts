6756c6274fc1155797ae14e819cfa769d08db57e
Tim Frey <frey.tim@outlook.com>
2019-04-30 20:15:12 +0200

    added a simple search algorithm

--- exercise/angular/Exercise02/bookstore/src/app/catalog/catalog.component.ts
+++ exercise/angular/Exercise02/bookstore/src/app/catalog/catalog.component.ts
@@ -10,11 +10,27 @@ import { Book, BookBinding } from './book';
 export class CatalogComponent {
   public books:Book[];
   public selectedBook:Book;
+  public keywords:string;
   constructor() {
     this.books = BOOK_DATA;
+    Book.prototype.toString = function toString() {
+      var ret = this.isbn+this.description+this.title+this.authors+this.publisher;
+      return ret;
+    }
   }
 
   public selectBook(book:Book){
     this.selectedBook = book;
+  }
+
+  public searchBooks(){
+    this.books = [];
+    var i = 0;
+    for(let book of BOOK_DATA){
+      if(book.toString().toLowerCase().search(this.keywords.toLowerCase())!=-1){
+        this.books[i] = book;
+        i++;
+      }
+    }
   }
 }
