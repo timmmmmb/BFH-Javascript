d2a20d156a542c013ff15e577d62a8520aa51d78
Tim Frey <frey.tim@outlook.com>
2019-04-30 18:46:49 +0200

    created template for the bookstore exercise

---
+++ exercise/angular/Exercise02/bookstore/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'bookstore',
+  templateUrl: 'app.component.html',
+  styles: []
+})
+export class AppComponent {
+  public title = 'Bookstore';
+}
