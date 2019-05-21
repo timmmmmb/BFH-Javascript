164c88d131d9e4653ea341025f2189d3558ace77
Tim Frey <frey.tim@oublook.com>
2019-05-21 19:10:17 +0200

    finished first part of exercise

--- exercise/angular/Exercise02/bookstore/src/app/app.module.ts
+++ exercise/angular/Exercise02/bookstore/src/app/app.module.ts
@@ -3,20 +3,34 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from '@angular/router'
 import { FormsModule } from '@angular/forms';
 import { CatalogComponent } from './catalog/catalog.component';
 import { BookDetailsComponent } from './catalog/book-details/book-details.component';
+import { HomeComponent } from './home/home.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'customer-details', component: CustomerDetailsComponent },
+  { path: '**', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     CatalogComponent,
-    BookDetailsComponent
+    BookDetailsComponent,
+    HomeComponent,
+    CustomerDetailsComponent
   ],
   imports: [
+    RouterModule,
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
